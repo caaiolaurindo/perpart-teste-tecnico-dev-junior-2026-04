2a3a2c056c188242080a019b20d428ae8f99b9ac
laurindolucas <caiolucax2024@gmail.com>
2026-04-22 16:29:39 -0300

    fix: corrige favorites.models por conta da instabilidade do docker

--- backend/src/favorites/favorites.module.ts
+++ backend/src/favorites/favorites.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FavoritesService } from './favorites.service';
-import { FavoritesController } from './favorites.controller';
 import { Favorite } from './favorite.entity';
+import { Product } from 'src/products/product.entity';
+import { Notification } from 'src/notifications/notification.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Favorite])],
+  imports: [
+    TypeOrmModule.forFeature([Favorite, Product, Notification]),
+  ],
   providers: [FavoritesService],
-  controllers: [FavoritesController],
 })
 export class FavoritesModule {}
