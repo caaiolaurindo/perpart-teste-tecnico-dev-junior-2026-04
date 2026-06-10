75a1613335e9a82bbf913af6656c732892ed06a2
caaiolaurindo <caio.llsilva@perpart.pe.gov.br>
2026-06-10 16:34:37 -0300

    feat: add mensagens de tratamento via console

--- backend/src/users/user.seed.ts
+++ backend/src/users/user.seed.ts
@@ -1,25 +1,27 @@
 import { DataSource } from 'typeorm';
 import { User, UserRole } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 export default class UserSeeder {
-  public async run(
-    dataSource: DataSource
-  ): Promise<any> {
+  public async run(dataSource: DataSource): Promise<any> {
     const repository = dataSource.getRepository(User);
+    const adminEmail = 'admin@email.com';
 
-    const adminExists = await repository.findOneBy({ email: 'admin@email.com' });
+    console.log('🧹 Limpando registros antigos do Admin para começar do zero...');
+  
+    await repository.delete({ email: adminEmail });
 
-    if (!adminExists) {
+    console.log('🔐 Gerando hash seguro para a nova senha...');
+    const hashedPassword = await bcrypt.hash('padraosenhaadmin', 10);
 
-      await repository.insert({
-        name: 'Administrador',
-        email: 'admin@email.com',
-        password: 'padraosenhaadmin',
-        role: UserRole.ADMIN,
-      });
-      console.log('✅ Usuário admin criado com sucesso!');
-    }else{
-      console.log('⚠️ Usuário admin já existe, pulando criação.');
-    }
+    console.log('🌱 Criando o novo usuário Administrador Padrão...');
+    await repository.insert({
+      name: 'Administrador',
+      email: adminEmail,
+      password: hashedPassword, 
+      role: UserRole.ADMIN,
+    });
+
+    console.log('✅ Tudo pronto! Usuário admin resetado e pronto para o Login.');
   }
 }
